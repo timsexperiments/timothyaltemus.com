1f2dd25b5fab01d128736bb2a84e164f24f9c4ef
Tim <76897954+timsexperiments@users.noreply.github.com>
2023-09-15 01:57:20 +0000

    Complete hero section

--- apps/timsexperiments/astro.config.mjs
+++ apps/timsexperiments/astro.config.mjs
@@ -3,7 +3,11 @@ import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
 import { defineConfig } from 'astro/config';
 
+import cloudflare from "@astrojs/cloudflare";
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [mdx(), react(), tailwind()],
+  output: "server",
+  adapter: cloudflare()
 });
